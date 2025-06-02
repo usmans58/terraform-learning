f6b091c30cff375cf3593b89180a3af7ea92cbe6
Usman Siddiqui <usmansiddiqui51@gmail.com>
2025-06-03 01:54:53 +0500

    s3 policy

--- 13. AWS/2. S3/main.tf
+++ 13. AWS/2. S3/main.tf
@@ -6,6 +6,26 @@ resource "aws_s3_bucket_object" "example_doc" {
   bucket = aws_s3_bucket.example.id
   key    = "example.doc"
   source = "example.doc"
-  
-  
+}
+
+data "aws_iam_group" "example-group-data" {
+  group_name = "example-group"
+}
+resource "aws_s3_bucket_policy" "example" {
+  bucket = aws_s3_bucket.example.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+        "Principal": {
+            "AWS": "${data.aws_iam_group.example-group-data.arn}"
+        },
+      "Action": "*",
+      "Resource": "${aws_s3_bucket.example.id}/*"
+    }
+  ]
+}
+EOF
 }
