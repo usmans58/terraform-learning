a2883819cc21ea99a90a932e458089e36e0cd10b
Usman Siddiqui <usmansiddiqui51@gmail.com>
2025-06-02 22:00:20 +0500

    aws policy

--- 13. AWS/1. IAM/main.tf
+++ 13. AWS/1. IAM/main.tf
@@ -4,3 +4,24 @@ resource "aws_iam_user" "name" {
       Description = "This is an example IAM user"
     }
 }
+resource "aws_iam_policy" "user_policy" {
+    name        = "adminUser"
+    description = "This policy grants permissions to the example user"
+    policy = <<EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": "*",
+          "Resource": "*"
+        }
+      ]
+    }
+    EOF
+}
+
+resource "aws_iam_user_policy_attachment" "user_policy_attachment" {
+    user       = aws_iam_user.name.name
+    policy_arn = aws_iam_policy.user_policy.arn
+}
