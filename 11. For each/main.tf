8d149bb3ea7c7883d849e0ac29f3edb2925966d3
Usman Siddiqui <usmansiddiqui51@gmail.com>
2025-06-15 06:41:15 +0500

    refactored code

--- 11. For each/main.tf
+++ 11. For each/main.tf
@@ -1,7 +1,8 @@
 resource "local_file" "name" {
+    for_each = var.filename
     filename = each.value
     content  = "This is a file named ${each.value}."
-    for_each = var.filename
+    
 
   
 }
